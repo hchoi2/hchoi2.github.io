c95d6fa07e753b0005eab6847072253cc8454c58
Hyejeong Choi <145830994+hchoi2@users.noreply.github.com>
2023-10-20 20:28:52 -0700

    Create convert_encoding.sh

---
+++ convert_encoding.sh
@@ -0,0 +1,14 @@
+find "$1" -name "*.php" -o -name "*.html" -o -name "*.jpg" -o -name "*.png" -o -name "*.gif" -o -name "*.GIF" -o -name "*.JPG" -o -name "*.PNG" -o -name "css" -o -name "*.js" -o -name "*.txt" | while read filename
+
+do
+
+tempName=${filename}~temp~.txt
+
+mv "$filename" "$tempName"
+
+iconv -c -f euc-kr -t utf-8 "$tempName" > "$filename"
+
+rm "$tempName"
+
+
+done
